1a878ec64589420340f2d2ec51e5cdae1deb51b2
cipher coco <jurgenlohu1@gmail.com>
2024-02-21 11:38:17 +0200

    uus projekt

---
+++ veebipood/src/App.js
@@ -0,0 +1,17 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <button className='btn'>Click me</button>
+      <img
+        className='turtle'
+        src='https://www.fisheries.noaa.gov/s3/styles/full_width/s3/dam-migration/hawksbill_sea_turtle.jpg?itok=ESbU98wo'
+        alt='turtle'
+      />
+    </div>
+  );
+}
+
+export default App;
