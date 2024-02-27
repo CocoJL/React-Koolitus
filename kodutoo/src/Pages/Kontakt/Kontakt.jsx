ad5d622383a974618d800e53c6854768301003fe
cipher coco <jurgenlohu1@gmail.com>
2024-02-27 13:51:50 +0200

    kontakt tehtud

--- kodutoo/src/Pages/Kontakt/Kontakt.jsx
+++ kodutoo/src/Pages/Kontakt/Kontakt.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
 import "./kontakt.css";
 
 function Kontakt() {
-  return <div></div>;
+  const [muudaNimi, setMuudaNimi] = useState("Tallinn");
+  const [muudaNumber, setMuudaNumber] = useState("5512932");
+  const [muudaEmail, setMuudaEmail] = useState("bla@gmail.com");
+  const [ingliseKeelne, setIngliseKeelne] = useState("ei");
+
+  const muudaKeel = () => {
+    setMuudaNimi("London");
+    setMuudaNumber("1233123");
+    setMuudaEmail("wagwan@london.com");
+    setIngliseKeelne("jah");
+  };
+  return (
+    <div>
+      <div>
+        {muudaNimi} <br />
+        {muudaNumber} <br />
+        {muudaEmail} <br />
+      </div>
+      <button onClick={muudaKeel}>Muuda inglise keelseks</button>
+      {ingliseKeelne === "jah" && <div>Leht on inglise keelne</div>}
+    </div>
+  );
 }
 
 export default Kontakt;
