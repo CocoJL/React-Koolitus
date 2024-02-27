7c6ba104387044861c5617e9b976277dea05e8ce
cipher coco <jurgenlohu1@gmail.com>
2024-02-27 11:56:43 +0200

    turnary operator

--- uudised/src/pages/Kontakt.js
+++ uudised/src/pages/Kontakt.js
@@ -6,24 +6,53 @@ function Kontakt() {
   const [kontaktKris, muudaKontaktKris] = useState(false);
   const [kontaktYlemiste, muudaKontaktYlemiste] = useState(false);
   const [kontaktTasku, muudaKontaktTasku] = useState(false);
+  const [valitud, setValitud] = useState(false);
   return (
     <div>
       <div>See on kontaktide leht, nähtav localhost:3000/kontakt aadressil</div>
-      <div>Võta meiega ühendust:</div>
+      <div
+        className={valitud === true ? "valitud" : undefined}
+        onClick={() => setValitud(!valitud)}
+      >
+        Võta meiega ühendust:
+      </div>
       <br />
 
-      <div onClick={() => muudaKontaktKris(!kontaktKris)}>Kristiine keskus</div>
-      {kontaktKris && <div>+351231231</div>}
-      <div>Endla 45</div>
+      <div
+        className={kontaktKris === true ? "valitud" : undefined}
+        onClick={() => muudaKontaktKris(!kontaktKris)}
+      >
+        Kristiine keskus
+      </div>
+      {kontaktKris && <div className='valitud'>+351231231</div>}
+      <div className={kontaktKris === true ? "valitud" : undefined}>
+        Endla 45
+      </div>
       <br />
 
-      <div onClick={() => muudaKontaktYlemiste(!kontaktYlemiste)}>Ülemiste keskus</div>
-      {kontaktYlemiste && <div>+34151231</div>}
-      <div>Suur-Sõjamäe 4</div>
+      <div
+        className={kontaktYlemiste === true ? "valitud" : undefined}
+        onClick={() => muudaKontaktYlemiste(!kontaktYlemiste)}
+      >
+        Ülemiste keskus
+      </div>
+      {kontaktYlemiste && <div className='valitud'>+34151231</div>}
+      <div className={kontaktYlemiste === true ? "valitud" : undefined}>
+        Suur-Sõjamäe 4
+      </div>
       <br />
-      <div onClick={() => muudaKontaktTasku(!kontaktTasku)}>Tasku keskus</div>
-      {kontaktTasku && <div>+39312323</div>}
-      <div>Turu 2</div>
+
+      <div
+        className={kontaktTasku === true ? "valitud" : undefined}
+        onClick={() => muudaKontaktTasku(!kontaktTasku)}
+      >
+        Tasku keskus
+      </div>
+      {kontaktTasku && <div className='valitud'>+39312323</div>}
+      <div className={kontaktTasku === true ? "valitud" : undefined}>
+        Turu 2
+      </div>
+
       <Link to='/avaleht'>
         <button>Tagasi</button>
       </Link>
